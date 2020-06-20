bc5953488bf14dec6eaf4eea797b3b4c871bf39b
Fabricyo Barreto <bass.barreto@gmail.com>
2020-06-20 20:20:20 -0300

    finalizado a criação/estilização do projeto clone

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-import { Container, Title, ExpandIcon } from './styles';
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
-const ServerName: React.FC = () => {
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+    if(div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
   return(
       <Container>
-          <Title>Servidor do Rodz</Title>
-          <ExpandIcon />
+        <Messages ref={messagesRef}>
+          {Array.from(Array(15).keys()).map((n) => (
+            <ChannelMessage
+            author="Fabricyo Barreto"
+            date="20/06/2020"
+            content="Hoje é meu aniversário"
+          />
+          ))}
+
+          <ChannelMessage
+            author="Lilian Barreto"
+            date="01/07/2020"
+            content={
+              <>
+                <Mention>@Fabricyo Barreto</Mention>, me carrega no CS:GO de novo por favor?
+              </>
+            }
+            hasMention
+            isBot
+          />
+
+        </Messages>
+
+        <InputWrapper>
+          <Input type="text" placeholder="Conversar em #chat-livre" />
+          <InputIcon />
+        </InputWrapper>          
       </Container>
   );
 }
 
-export default ServerName;
+export default ChannelData;
